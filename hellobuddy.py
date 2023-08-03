2398a3a51dbb71675169461d58981f87d0f899a6
theokayauk <kevingibbons196883@gmail.com>
2023-08-03 14:57:36 -0400

    MORE

--- hellobuddy.py
+++ hellobuddy.py
@@ -6,6 +6,14 @@ from flask import Response
 from flask import request as flask_request
 import messagefinder
 
+import logging
+import os
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+bot_token = 'xoxb-145080091058-5433144011491-7SYgz3ZgdARIykMx10lLxvl0'
+
 
 app = Flask(__name__)
 
@@ -15,7 +23,11 @@ def hello():
 
 @app.route('/saysomething', methods=['POST'])
 def helloo():
-    return "Hello, World"
+    client = WebClient(token= bot_token)
+
+    payload = flask_request.get_json()
+
+    mess = client.chat_postMessage(channel='C05KW8HL6SZ', text='hey buddy')
 
     
 if __name__ == '__main__':
